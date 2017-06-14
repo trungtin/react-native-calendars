c17467aad0379ba79f981fa0b7a018e69bd30da2
Trần Trung Tín <trungtindev@gmail.com>
2017-06-14 16:38:04 +0700

    restyling

--- src/calendar/style.js
+++ src/calendar/style.js
@@ -15,8 +15,8 @@ export default function getStyle(theme={}) {
       backgroundColor: appStyle.calendarBackground
     },
     week: {
-      marginTop: 7,
-      marginBottom: 7,
+      marginTop: 3,
+      marginBottom: 3,
       flexDirection: 'row',
       justifyContent: 'space-around'
     },
